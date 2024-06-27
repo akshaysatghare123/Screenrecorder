9a924060e622b278b3a8f071c567f595d713d13f
akshaysatghare123 <59027315+akshaysatghare123@users.noreply.github.com>
2024-06-27 14:12:56 +0530

    Add files via upload

---
+++ init.js
@@ -0,0 +1,28 @@
+
+
+const start =async()=>{
+
+    const stream=await navigator.mediaDevices.getDisplayMedia(
+    {
+        video:{
+            mediaSource : "screen",
+        },
+    });
+    const data=[];
+
+    const mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.ondataavailable = (e) => {
+    data.push(e.data);
+    };
+    mediaRecorder.start();
+    mediaRecorder.onstop = (e) => {
+        document.querySelector("video").src=URL.createObjectURL(
+
+        new Blob(data, {
+
+            type:data[0].type,
+        })
+        );
+    };
+};
+start();
